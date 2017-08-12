9fd8925908b423ef194c74fc2ef5ac918314ded6
Dwight Ware <dwightware4@gmail.com>
2017-08-12 14:46:40 -0700

    removed some test code that isn't part of the actual boilerplate

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,30 +4,9 @@ import { render } from 'react-dom';
 import './Root.scss';
 
 class Root extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      count: 0,
-    };
-  }
-
   render() {
-    return (
-      <div>
-        <div>
-          {this.state.count}
-        </div>
-        <button onClick={this.increaseCount}>Click</button>
-      </div>
-    );
+    return <div>Hello React!</div>;
   }
-
-  increaseCount = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
 }
 
 render(<Root />, document.getElementById('root'));
